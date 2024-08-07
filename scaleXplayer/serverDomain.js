fb92bdac2ac7558bdcba800afff60cf561974488
Gary <46955710+garywu007@users.noreply.github.com>
2024-08-07 11:11:55 -0500

    Update serverDomain.js

--- scaleXplayer/serverDomain.js
+++ scaleXplayer/serverDomain.js
@@ -1,2 +1,3 @@
-var serverDomain = "https://toolbox-garywu007.koyeb.app"; 
+var serverDomain = "https://gary.webhop.me:9200"; 
 // "toolbox-fmdzx.ondigitalocean.app"; 
+// "https://toolbox-garywu007.koyeb.app";
